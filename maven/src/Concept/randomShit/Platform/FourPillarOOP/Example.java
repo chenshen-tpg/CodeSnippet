5a84e2ae14fdf5f1309a081849c96fc3f633f687
chen shen <117608047+chenshen-tpg@users.noreply.github.com>
2026-05-06 09:52:35 +1000

    6/5/2026 update some QA

---
+++ maven/src/Concept/randomShit/Platform/FourPillarOOP/Example.java
@@ -0,0 +1,28 @@
+package Concept.randomShit.Platform.FourPillarOOP;
+
+public class Example {
+
+    //Encapsulation, having object class and getter&setter method
+    public class User {
+        private String name;
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+    }
+
+    //Abstraction, having a interface that can be overridden.
+    public interface PaymentProcessor {
+        void processPayment(double amount);
+    }
+
+    //Inheritance, get properties from other class.
+    public class CreditCardPayment implements PaymentProcessor {
+        public void processPayment(double amount) {
+            // Implementation
+        }
+    }
+
+    //Polymorphism same method name for different parameters taken
+    public void makePayment(PaymentProcessor processor, double amount) {
+        processor.processPayment(amount);
+    }
+}
