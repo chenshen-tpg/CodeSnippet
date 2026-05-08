2a71c6c39dc9fd16150d6f76406cfe32dace3216
chen shen <117608047+chenshen-tpg@users.noreply.github.com>
2026-05-08 13:21:42 +1000

    8/5/2026 update some files.

--- maven/src/main/java/CodingQuestions/Algorithms/Medium/RotatingTheBox_1861/TEST.java
+++ maven/src/main/java/CodingQuestions/Algorithms/Medium/RotatingTheBox_1861/TEST.java
@@ -1,6 +1,51 @@
 package CodingQuestions.Algorithms.Medium.RotatingTheBox_1861;
 
 public class TEST {
+    /***
+     *
+     You are given an m x n matrix of characters boxGrid representing a side-view of a box. Each cell of the box is one of the following:
+
+     A stone '#'
+     A stationary obstacle '*'
+     Empty '.'
+     The box is rotated 90 degrees clockwise, causing some of the stones to fall due to gravity. Each stone falls down until it lands on an obstacle, another stone, or the bottom of the box. Gravity does not affect the obstacles' positions, and the inertia from the box's rotation does not affect the stones' horizontal positions.
+
+     It is guaranteed that each stone in boxGrid rests on an obstacle, another stone, or the bottom of the box.
+
+     Return an n x m matrix representing the box after the rotation described above.
+
+
+     Example 1:
+
+     Input: boxGrid = [["#",".","#"]]
+     Output: [["."],
+              ["#"],
+              ["#"]]
+     */
+    public static void main(String[] args) {
+        char[][] boxGrid1 = {
+                {'#','#','*','.','*','.'},
+                {'#','#','#','*','.','.'},
+                {'#','#','#','.','#','.'}
+        };
+        TEST obj = new TEST();
+        obj.display(obj.rotateOnly(boxGrid1));
+        System.out.println("Rotation of 90 degree being like: ");
+        obj.display(obj.rotateTheBox90(boxGrid1));
+        System.out.println("Rotation of 90 degree with top-down gravity simulation being like: ");
+        obj.display(obj.rotateTheBox90TopDown(boxGrid1));
+    }
+    public char[][] rotateOnly(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+        char[][] newGrid = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                newGrid[j][m - i - 1] = boxGrid[i][j];
+            }
+        }
+        return newGrid;
+    }
     public char[][] rotateTheBox90(char[][] boxGrid) {
         int m = boxGrid.length;
         int n = boxGrid[0].length;
@@ -10,26 +55,58 @@ public class TEST {
                 newGrid[j][m - i - 1] = boxGrid[i][j];
             }
         }
-        return newGrid;
-    }
-    public char[][] rotateTheBox180(char[][] boxGrid) {
-        int m = boxGrid.length;
-        int n = boxGrid[0].length;
-        char [][] newGrid = new char [m][n]; // should be m x n
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                newGrid[m - i - 1][n - j - 1] = boxGrid[i][j];
+        //{'#','#','*','.','*','.'},
+        for (int col = 0; col < m; col++) {
+            int write = n - 1;
+            for (int row = n - 1; row >= 0; row--) {
+                if (newGrid[row][col] == '*') {
+                    write = row - 1;
+                } else if (newGrid[row][col] == '#') {
+                    if (row != write) {
+                        newGrid[row][col] = '.';
+                        newGrid[write][col] = '#';
+                    }
+                    write--;
+                }
             }
         }
         return newGrid;
     }
-    public char[][] rotateTheBox270(char[][] boxGrid) {
+    // Top-down gravity simulation for a rotated box
+    public char[][] rotateTheBox90TopDown(char[][] boxGrid) {
         int m = boxGrid.length;
         int n = boxGrid[0].length;
-        char [][] newGrid = new char [n][m];
-        for (int i = 0; i < m;i++) {
-            for (int j = 0; j < n;j++) {
-                newGrid[n - j - 1][i] = boxGrid[i][j];
+        char[][] newGrid = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                newGrid[j][m - i - 1] = boxGrid[i][j];
+            }
+        }
+        // Top-down gravity simulation for each column {'#','#','*','.','*','.'},
+        for (int col = 0; col < m; col++) {
+            int start = 0;
+            while (start < n) {
+                if (newGrid[start][col] == '*') {
+                    start++;
+                    continue;
+                }
+                int end = start;
+                while (end < n && newGrid[end][col] != '*') {
+                    end++;
+                }
+                int stoneCount = 0;
+                for (int row = start; row < end; row++) {
+                    if (newGrid[row][col] == '#') stoneCount++;
+                }
+                for (int row = end - 1; row >= start; row--) {
+                    if (stoneCount > 0) {
+                        newGrid[row][col] = '#';
+                        stoneCount--;
+                    } else {
+                        newGrid[row][col] = '.';
+                    }
+                }
+                start = end + 1;
             }
         }
         return newGrid;
@@ -41,24 +118,5 @@ public class TEST {
             }
             System.out.println();
         }
-    }
-
-    public static void main(String[] args) {
-        char[][] boxGrid1 = {
-                {'#', '.', '*'},
-                {'.', '#', '.'}
-        };
-        TEST obj = new TEST();
-        System.out.println("Rotation of 90 degree being like: ");
-        char[][] rotated90 = obj.rotateTheBox90(boxGrid1);
-        obj.display(rotated90);
-        System.out.println();
-        System.out.println("Rotation of 180 degree being like: ");
-        char[][] rotated180 = obj.rotateTheBox180(boxGrid1);
-        obj.display(rotated180);
-        System.out.println();
-        System.out.println("Rotation of 270 degree being like: ");
-        char[][] rotated270 = obj.rotateTheBox270(boxGrid1);
-        obj.display(rotated270);
     }
 }
---
+++ maven/src/main/java/CodingQuestions/Algorithms/Medium/RotatingTheBox_1861/testMyself.java
@@ -0,0 +1,21 @@
+package CodingQuestions.Algorithms.Medium.RotatingTheBox_1861;
+
+public class testMyself {
+    public static void main(String[] args) {
+        char [] arr = new char [] {'#','#','#','.','#','.'};
+
+        int len = arr.length;
+        int curIndex = len - 1;
+        for (int i = len - 1; i >= 0; i--) {
+            if (arr[i] == '*') {
+                curIndex = i - 1;
+            }
+            if (i != curIndex) {
+                arr[i] = '.';
+                arr[curIndex] = '#';
+            }
+            curIndex--;
+        }
+        System.out.println(arr);
+    }
+}
