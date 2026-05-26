e1e5b0a298c8796b7ad023ebbbad67d714dc10ea
chen.shen <chen.shen@tgptelecom.com.au>
2026-05-26 17:47:36 +1000

    26/5/2026 update

---
+++ maven/src/main/CodingQuestions/Algorithms/Easy/CountSpecialChars_3120/SampleAns.java
@@ -0,0 +1,36 @@
+package main.CodingQuestions.Algorithms.Easy.CountSpecialChars_3120;
+
+public class SampleAns {
+    public static void main(String[] args) {
+
+    }
+    public int numberOfSpecialChars(String word) {
+
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+        // Traverse string
+        for (char ch : word.toCharArray()) {
+
+            // lowercase
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            }
+
+            // uppercase
+            else {
+                upper[ch - 'A'] = true;
+            }
+        }
+
+        int count = 0;
+        // Check both exist
+        for (int i = 0; i < 26; i++) {
+
+            if (lower[i] && upper[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
