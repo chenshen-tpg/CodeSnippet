444b090c877438012a14aa358505a072d9933b45
chen shen <117608047+chenshen-tpg@users.noreply.github.com>
2026-05-28 09:19:50 +1000

    28/5/2026 update the memo with extra pointer.

---
+++ maven/src/main/CodingQuestions/Algorithms/Easy/DesignOrderedSteam_1656/ArrayVersion.java
@@ -0,0 +1,23 @@
+package main.CodingQuestions.Algorithms.Easy.DesignOrderedSteam_1656;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayVersion {
+    String[] stream;
+    int ptr = 1;
+
+    public ArrayVersion(int n) {
+        stream = new String[n + 1]; // 1-based indexing
+    }
+
+    public List<String> insert(int idKey, String value) {
+        List<String> res = new ArrayList<>();
+        stream[idKey] = value;
+        while (ptr < stream.length && stream[ptr] != null) {
+            res.add(stream[ptr]);
+            ptr++;
+        }
+        return res;
+    }
+}
---
+++ maven/src/main/CodingQuestions/Algorithms/Easy/DesignOrderedSteam_1656/HashMapVersion.java
@@ -0,0 +1,24 @@
+package main.CodingQuestions.Algorithms.Easy.DesignOrderedSteam_1656;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HashMapVersion {
+    int index = 1;
+    Map<Integer, String> hm;
+    public HashMapVersion(int n) {
+        hm = new HashMap();
+    }
+
+    public List<String> insert(int idKey, String value) {
+        List<String> li = new ArrayList();
+        hm.put(idKey, value);
+        while (hm.containsKey(index)) {
+            li.add(hm.get(index));
+            index++;
+        }
+        return li;
+    }
+}
